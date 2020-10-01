c62b491abf068b612389f3e61ecbedd5b5b1f583
Minh Yến <phamminhyen97@gmail.com>
2020-10-01 18:51:22 +0700

    update code 01/10/2020

--- ManagementData.Web/Controllers/DataController.cs
+++ ManagementData.Web/Controllers/DataController.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Drawing;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Text;
@@ -48,6 +49,36 @@ namespace ManagementData.Web.Controllers
             }
             return RedirectToAction<HomeController>(nameof(Index));
         }
+
+        [HttpPost]
+        public async Task<ActionResult> DeleteAllData(IEnumerable<int> dataItems, string admin)
+        {
+            try
+            {
+                foreach (var item in dataItems)
+                {
+                    await db.Database.ExecuteSqlCommandAsync("EXEC Proc_DeleteData  @id ", new SqlParameter { ParameterName = "id", Value = item });
+                }
+
+                if (!string.IsNullOrEmpty(admin))
+                {
+                    return RedirectToAction<AdminController>(nameof(Index));
+                }
+                return RedirectToAction<HomeController>(nameof(Index));
+            }
+            catch (Exception)
+            {
+
+                if (!string.IsNullOrEmpty(admin))
+                {
+                    return RedirectToAction<AdminController>(nameof(Index));
+                }
+                return RedirectToAction<HomeController>(nameof(Index));
+            }
+
+        }
+
+
 
         [HttpPost]
         public async Task<ActionResult> UpdateData(string text, int id, string admin)
@@ -107,10 +138,6 @@ namespace ManagementData.Web.Controllers
                 var search = string.Empty;
                 int length = Convert.ToInt32(Request["length"]) < 0 ? 50 : Convert.ToInt32(Request["length"]);
                 page = (start / length) + 1;
-
-
-                //var datas = await db.Database
-                //  .SqlQuery<DataInsert>($"SELECT* FROM DataInserts where UserId = '{userId}' ORDER BY TimeCreate desc  OFFSET({page} - 1) * {length} ROWS FETCH NEXT {length} ROWS ONLY").ToListAsync();
                 var datas = await db.Database
                   .SqlQuery<DataDto>($"SELECT* FROM DataInserts where UserId = '{userId}' ORDER BY TimeCreate desc  OFFSET({page} - 1) * {length} ROWS FETCH NEXT {length} ROWS ONLY").ToListAsync();
 
@@ -121,13 +148,7 @@ namespace ManagementData.Web.Controllers
                     datas[i].Stt = i + 1 + (page - 1) * length;
                     datas[i].TimeString = datas[i].TimeCreate.ToString("yyyy-MM-dd hh:mm:ss.fff");
                 }
-                //var datas = await db.Database
-                //      .SqlQuery<DataInsert>("PROC_GetData_Paging  @page , @size, @userId, @orderId, @search",
-                //      new SqlParameter { ParameterName = "userId", Value = userId },
-                //      new SqlParameter { ParameterName = "page", Value = page },
-                //      new SqlParameter { ParameterName = "size", Value = length },
-                //      new SqlParameter { ParameterName = "orderId", Value = orderSort },
-                //      new SqlParameter { ParameterName = "search", Value = search }).ToListAsync();
+
                 return Json(new { dataObject = datas, idlast = idLast, page = page, size = length, orderSort = orderSort }, JsonRequestBehavior.AllowGet);
 
             }
@@ -140,82 +161,98 @@ namespace ManagementData.Web.Controllers
         }
 
         [HttpGet]
-        public async Task<FileResult> Export(string userId, int page = 1, int size = 10, int orderSort = 0)
+        public async Task<FileResult> Export(string userId, int page = 1, int size = 10, int orderSort = 0, string windowId = "")
         {
             try
             {
-                var datas = await db.Database
-                   .SqlQuery<DataInsert>("PROC_GetData_Paging  @page , @size, @userId, @orderId, @search",
-                   new SqlParameter { ParameterName = "userId", Value = userId },
-                   new SqlParameter { ParameterName = "page", Value = page },
-                   new SqlParameter { ParameterName = "size", Value = size },
-                   new SqlParameter { ParameterName = "orderId", Value = orderSort },
-                   new SqlParameter { ParameterName = "search", Value = string.Empty }).ToListAsync();
-
-                string fileName = "DataExport" + DateTime.Now.ToString("ddMMyyyy_HHmmss") + ".xlsx";
-                ExcelPackage.LicenseContext = LicenseContext.Commercial;
-                using (ExcelPackage p = new ExcelPackage())
+                //var datas = await db.Database
+                //   .SqlQuery<DataInsert>("PROC_GetData_Paging  @page , @size, @userId, @orderId, @search",
+                //   new SqlParameter { ParameterName = "userId", Value = userId },
+                //   new SqlParameter { ParameterName = "page", Value = page },
+                //   new SqlParameter { ParameterName = "size", Value = size },
+                //   new SqlParameter { ParameterName = "orderId", Value = orderSort },
+                //   new SqlParameter { ParameterName = "search", Value = string.Empty }).ToListAsync();
+                using (var dbstransaction = db.Database.BeginTransaction())
                 {
-                    //Here setting some document properties
-                    p.Workbook.Properties.Author = "DataLarge";
-                    p.Workbook.Properties.Title = "Danh sách data";
+                    try
+                    {
+                        var datas = await db.Database
+                                    .SqlQuery<DataInsert>($"SELECT * FROM DataInserts WHERE UserId='{userId}'").ToListAsync();
 
-                    //Create a sheet
-                    p.Workbook.Worksheets.Add("Danh sách");
-                    ExcelWorksheet ws = p.Workbook.Worksheets.FirstOrDefault();
-                    ws.Name = "Danh sách"; //Setting Sheet's name
-                    ws.Cells.Style.Font.Size = 11; //Default font size for whole sheet
-                    ws.Cells.Style.Font.Name = "Calibri"; //Default Font name for whole sheet
+                        string fileName = "DataExport" + DateTime.Now.ToString("ddMMyyyy_HHmmss") + ".xlsx";
+                        ExcelPackage.LicenseContext = LicenseContext.Commercial;
+                        using (ExcelPackage p = new ExcelPackage())
+                        {
+                            //Here setting some document properties
+                            p.Workbook.Properties.Author = "DataLarge";
+                            p.Workbook.Properties.Title = "Danh sách data";
 
-                    // Create header column
-                    string[] arrColumnHeader = {    "STT",
+                            //Create a sheet
+                            p.Workbook.Worksheets.Add("Danh sách");
+                            ExcelWorksheet ws = p.Workbook.Worksheets.FirstOrDefault();
+                            ws.Name = "Danh sách"; //Setting Sheet's name
+                            ws.Cells.Style.Font.Size = 11; //Default font size for whole sheet
+                            ws.Cells.Style.Font.Name = "Calibri"; //Default Font name for whole sheet
+
+                            // Create header column
+                            string[] arrColumnHeader = {    "STT",
                                                 "Id",
                                                 "Text",
                                                 "CreateDate",
                                                 };
-                    var countColHeader = arrColumnHeader.Count();
-                    int colIndex = 1;
-                    int rowIndex = 1;
+                            var countColHeader = arrColumnHeader.Count();
+                            int colIndex = 1;
+                            int rowIndex = 1;
 
-                    //Creating Headings
-                    foreach (var item in arrColumnHeader)
-                    {
-                        var cell = ws.Cells[rowIndex, colIndex];
+                            //Creating Headings
+                            foreach (var item in arrColumnHeader)
+                            {
+                                var cell = ws.Cells[rowIndex, colIndex];
 
-                        //Setting the background color of header cells to Gray
-                        var fill = cell.Style.Fill;
-                        fill.PatternType = ExcelFillStyle.Solid;
-                        fill.BackgroundColor.SetColor(Color.LightBlue);
+                                //Setting the background color of header cells to Gray
+                                var fill = cell.Style.Fill;
+                                fill.PatternType = ExcelFillStyle.Solid;
+                                fill.BackgroundColor.SetColor(Color.LightBlue);
 
-                        //Setting Top/left,right/bottom borders.
-                        var border = cell.Style.Border;
-                        border.Bottom.Style =
-                            border.Top.Style =
-                            border.Left.Style =
-                            border.Right.Style = ExcelBorderStyle.Thin;
+                                //Setting Top/left,right/bottom borders.
+                                var border = cell.Style.Border;
+                                border.Bottom.Style =
+                                    border.Top.Style =
+                                    border.Left.Style =
+                                    border.Right.Style = ExcelBorderStyle.Thin;
 
-                        //Setting Value in cell
-                        cell.Value = item;
+                                //Setting Value in cell
+                                cell.Value = item;
 
-                        colIndex++;
+                                colIndex++;
+                            }
+
+                            // Adding Data into rows
+                            int stt = 1;
+                            foreach (var item in datas)
+                            {
+                                colIndex = 1;
+                                rowIndex++;
+                                //Setting Value in cell
+                                ws.Cells[rowIndex, colIndex++].Value = stt++;
+                                ws.Cells[rowIndex, colIndex++].Value = item.Id;
+                                ws.Cells[rowIndex, colIndex++].Value = item.Text;
+                                ws.Cells[rowIndex, colIndex++].Value = item.TimeCreate.ToString("yyyy-MM-dd hh:mm:ss.fff");
+                            }
+
+                            // Generate A File with name
+                            Byte[] bin = p.GetAsByteArray();
+                            await db.Database.ExecuteSqlCommandAsync($"DELETE FROM DataInserts WHERE UserId='{userId}'");
+                            dbstransaction.Commit();
+                            Response.Cookies.Add(new HttpCookie(windowId, DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss:ff")));
+                            return File(bin, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
+                        }
                     }
-
-                    // Adding Data into rows
-                    int stt = 1;
-                    foreach (var item in datas)
+                    catch (Exception)
                     {
-                        colIndex = 1;
-                        rowIndex++;
-                        //Setting Value in cell
-                        ws.Cells[rowIndex, colIndex++].Value = stt++;
-                        ws.Cells[rowIndex, colIndex++].Value = item.Id;
-                        ws.Cells[rowIndex, colIndex++].Value = item.Text;
-                        ws.Cells[rowIndex, colIndex++].Value = item.TimeCreate.ToString("yyyy-MM-dd hh:mm:ss.fff");
+                        dbstransaction.Rollback();
+                        return null;
                     }
-
-                    // Generate A File with name
-                    Byte[] bin = p.GetAsByteArray();
-                    return File(bin, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
                 }
             }
             catch (Exception ex)
@@ -268,34 +305,71 @@ namespace ManagementData.Web.Controllers
             {
                 HttpPostedFileBase file = Request.Files[0];
 
-                ExcelPackage.LicenseContext = LicenseContext.Commercial;
-                using (ExcelPackage excelPackage = new ExcelPackage(file.InputStream))
+                var path = Path.GetExtension(file.FileName);
+
+
+                switch (path)
                 {
-                    ExcelWorksheet worksheet = excelPackage.Workbook.Worksheets.FirstOrDefault();
-                    int rows = worksheet.Dimension.Rows; // 20
-                    int columns = worksheet.Dimension.Columns; // 7
-                    // loop through the worksheet rows and columns
-                    for (int i = rows; i > 0; i--)
-                    {
-                        try
-                        {
-                            var text = worksheet.Cells[i, 1].Value.ToString();
-                            await db.Database.ExecuteSqlCommandAsync("EXEC Proc_InsertDataPost  @userId , @text, @timeCreate",
-                                new SqlParameter { ParameterName = "userId", Value = userId },
-                                new SqlParameter { ParameterName = "text", Value = text },
-                                new SqlParameter { ParameterName = "timeCreate", Value = DateTime.Now });
-                        }
-                        catch (Exception ex)
+                    case ".txt":
+                        using (var stream = new StreamReader(file.InputStream, Encoding.UTF8))
                         {
 
+                            while (stream.Peek() >= 0)
+                            {
+                                var line = stream.ReadLine();
+                                if (!string.IsNullOrEmpty(line))
+                                {
+                                    await db.Database.ExecuteSqlCommandAsync("EXEC Proc_InsertDataPost  @userId , @text, @timeCreate",
+                                       new SqlParameter { ParameterName = "userId", Value = userId },
+                                       new SqlParameter { ParameterName = "text", Value = line },
+                                       new SqlParameter { ParameterName = "timeCreate", Value = DateTime.Now });
+                                }
+                            }
+
+                            if (!string.IsNullOrEmpty(admin))
+                            {
+                                return RedirectToAction<AdminController>(nameof(Index));
+                            }
+                            return RedirectToAction<HomeController>(nameof(Index));
                         }
-                    }
-                    if (!string.IsNullOrEmpty(admin))
-                    {
-                        return RedirectToAction<AdminController>(nameof(Index));
-                    }
-                    return RedirectToAction<HomeController>(nameof(Index));
-                };
+                    case ".xls":
+                    case ".xlsx":
+                        // Excel
+                        ExcelPackage.LicenseContext = LicenseContext.Commercial;
+                        using (ExcelPackage excelPackage = new ExcelPackage(file.InputStream))
+                        {
+                            ExcelWorksheet worksheet = excelPackage.Workbook.Worksheets.FirstOrDefault();
+                            int rows = worksheet.Dimension.Rows; // 20
+                            int columns = worksheet.Dimension.Columns; // 7
+                                                                       // loop through the worksheet rows and columns
+                            for (int i = 1; i <= rows; i++)
+                            {
+                                try
+                                {
+                                    var text = worksheet.Cells[i, 1].Value.ToString();
+                                    await db.Database.ExecuteSqlCommandAsync("EXEC Proc_InsertDataPost  @userId , @text, @timeCreate",
+                                        new SqlParameter { ParameterName = "userId", Value = userId },
+                                        new SqlParameter { ParameterName = "text", Value = text },
+                                        new SqlParameter { ParameterName = "timeCreate", Value = DateTime.Now });
+                                }
+                                catch (Exception ex)
+                                {
+
+                                }
+                            }
+                            if (!string.IsNullOrEmpty(admin))
+                            {
+                                return RedirectToAction<AdminController>(nameof(Index));
+                            }
+                            return RedirectToAction<HomeController>(nameof(Index));
+                        };
+                    default:
+                        if (!string.IsNullOrEmpty(admin))
+                        {
+                            return RedirectToAction<AdminController>(nameof(Index));
+                        }
+                        return RedirectToAction<HomeController>(nameof(Index));
+                }
             }
             catch (Exception ex)
             {
